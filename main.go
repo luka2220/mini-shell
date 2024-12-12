d652d007e7e247cb93cb24c184fb87b262e1025e
Luka Piplica <42144047+luka2220@users.noreply.github.com>
2024-12-12 13:39:40 -0500

    shell input working

--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+var (
+	logger = log.New(os.Stdout, "[OUT]: ", log.Llongfile)
+)
+
+/*
+	Basic Command Execution: Run programs by name (e.g., ls, pwd).
+	Built-in Commands: Implement a few built-in commands like cd, exit, etc.
+	I/O Redirection: Support input (<) and output (>) redirection.
+	Piping: Implement simple piping (|) between commands.
+	Error Handling: Gracefully handle invalid commands or incorrect syntax.
+*/
 
 func main() {
-	fmt.Println("Mini shell writting in go")
+	fmt.Print("Mini-shell\n- To executre a command enter it in the prompt\n- To quit enter 'exit' or press ctrl+c\n\n")
+
+	read()
+}
+
+func read() {
+	buffer := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("> ")
+
+		cmd, err := buffer.ReadString('\n')
+		if err != nil {
+			logger.Printf("Error reading command: %v\n", err)
+			continue
+		}
+
+		if len(cmd) > 64 {
+			fmt.Println("Too long, the command has a max size of 64")
+			continue
+		}
+
+		switch cmd {
+		case "exit\n":
+			os.Exit(1)
+		default:
+			fmt.Printf("%s is an unrecognized command...\n", cmd[:len(cmd)-1])
+		}
+	}
 }
